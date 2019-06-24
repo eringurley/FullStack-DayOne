e2846bd847010bc0c0af4e730cdb223ef1072910
Erin Gurley <erinmgurley@gmail.com>
2019-06-24 12:02:09 -0700

    created dog test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const createDog = require('./create-dog');
+
+const args = process.argv;
+const dog = createDog(name, age, weight);
+
+const name = process.argv[0];
+const age = process.argv[1];
+const weight = process.argv[2];
+
+
+console.log(process.argv);
