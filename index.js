acd1f4e6e52b68165acf323f397a0260108243db
Erin Gurley <erinmgurley@gmail.com>
2019-06-24 10:59:24 -0700

    node exercise 1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hi');
+function createDog(name, age, weight) {
+    return {
+        name, 
+        age, 
+        weight
+    };
+}
+
+const dog = createDog('spot', 5, '20lbs'); 
+
+console.log(dog);
+
+// best way: 
+// const createDog = (name, age, weight) => ({
+//   name, 
+//   age, 
+//   weight
+// });
+// const dog = createDog('spot', 5, '20lbs'); 
+
+// console.log(dog);
